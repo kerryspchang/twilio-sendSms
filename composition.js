366f34c462cd6f3f11128ed3c504c2d8cfcf25d3
kerryspchang <kerryspchang@gmail.com>
2018-04-09 18:39:14 -0400

    Add files via upload

---
+++ composition.js
@@ -0,0 +1,1 @@
+composer.sequence(`/${namespace}/${projectName}/sendSms`)
---
+++ shellConfig.js
@@ -0,0 +1,11 @@
+{
+	"Twilio":{
+		"instructions":"This example uses Twilio to send text messages. You need a Twilio SID, token, and a Twilio phone number. Log in to your Twilio account, and create/select a Programmable SMS project. Your SID and token should be shown in the console home page (Dashboard). Your phone number can be found using the upper right \"Go to...\" textbox, and search for \"Phone numbers\"",
+		"source":"https://www.twilio.com/console",
+		"input":[
+			{"name":"ssid","label":"Account SID"},
+			{"name":"token","label":"Auth token"},
+			{"name":"from","label":"Twilio phone number (format: +19876543210)"}
+		]
+	}
+}
